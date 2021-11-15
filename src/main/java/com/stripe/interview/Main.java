77583cf8790bec8e842f717a664fc84b2fa23626
nathanwang <wang.p@husky.neu.edu>
2021-11-14 23:03:07 -0800

    test commit

--- src/main/java/com/stripe/interview/Main.java
+++ src/main/java/com/stripe/interview/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	public static void main(String... args) {
 		System.out.println("Hello world!");
 		System.out.println("test for personal github");
+		System.out.println("Final test after getting new intellij");
 	}
 
 	public static String useGuavaForSomeReason(String input) {
